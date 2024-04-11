6aa6de3a45bba2f25c81fcb2621a563e8c0632ac
munyasya21 <munyasyacynthia@gmail.com>
2024-04-11 22:15:21 +0300

    sign up

--- my-app/src/pages/LogIn.js
+++ my-app/src/pages/LogIn.js
@@ -1,63 +1,67 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 import books2 from "../assets/images/books2.jpg";
 
 const LogIn = () => {
   return (
-    <div className=' flex p-36 h-40 w-full object-cover text-white font-serif'>
-       
-        
-  <div class="p-4 justify-center items-center">
-    <h2 class="font-bold text-3xl mb-2 pt-10 ">Welcome Back</h2>
-    <p class="text-gray-600">Don't have an account yet?  
-    <a href="#" class="font-bold text-white hover:text-blue-600 pl-2">Sign up</a> </p>
-    <div className="mb-6 pt-8">
-            <label
-              htmlFor="email"
-              className="text-white mb-2 block text-sm font-medium"
-            >
-              Your Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              required
-              className="bg-white border pl-3 w-full border-black placeholder-black text-white  text-lg rounded-lg sm:w-[300px] lg:w-96 pt-2.5"
-              placeholder="Your E-mail"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="text-white mb-2 block text-sm font-medium"
-            >
-              Your Email
-            </label>
-            <input
-              type="password"
-              id="password"
-              required
-              className="bg-white border pl-3 w-full border-black placeholder-black text-gray  text-lg rounded-lg sm:w-[300px] lg:w-96 pt-2.5"
-              placeholder="Your Password"
-            />
-          </div>
-          <div className='  pl-2'>
-    <button class="mt-4  bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-40  rounded-xl justify-center items-center ">LogIn</button></div>
-  </div>
+    <div className=" flex p-36 h-40 w-full object-cover text-white font-serif">
+      <div class="p-4 justify-center items-center">
+        <h2 class="font-bold text-3xl mb-2 pt-10 ">Welcome Back</h2>
+        <p class="text-gray-600">
+          Don't have an account yet?
+          <a href="Signup" class="font-bold text-white hover:text-blue-600 pl-2">
+            Sign up
+          </a>{" "}
+        </p>
+        <div className="mb-6 pt-8">
+          <label
+            htmlFor="email"
+            className="text-white mb-2 block text-sm font-medium"
+          >
+            Your Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            required
+            className="bg-white border pl-3 w-full border-black placeholder-black text-white  text-lg rounded-lg sm:w-[300px] lg:w-96 pt-2.5"
+            placeholder="Your E-mail"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="text-white mb-2 block text-sm font-medium"
+          >
+            Your Email
+          </label>
+          <input
+            type="password"
+            id="password"
+            required
+            className="bg-white border pl-3 w-full border-black placeholder-black text-gray  text-lg rounded-lg sm:w-[300px] lg:w-96 pt-2.5"
+            placeholder="Your Password"
+          />
+        </div>
+        <div className="  pl-2">
+          <button class="mt-4  bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-40  rounded-xl justify-center items-center ">
+            LogIn
+          </button>
+        </div>
+      </div>
 
-{/* image to the right */}
-<div className='pl-56 pt-10  relative  '>
-    <Image src={books2}
-    width={740}
-    height={640}
-    className=' rounded-full object-cover transition-transform duration-700 transform hover:scale-125'
-    alt='books2 image'
-   
-    />
- 
-</div>
+      {/* image to the right */}
+      <div className="pl-56 pt-10  relative  ">
+        <Image
+          src={books2}
+          width={740}
+          height={640}
+          className=" rounded-full object-cover transition-transform duration-700 transform hover:scale-125"
+          alt="books2 image"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
--- my-app/src/pages/Signup.js
+++ my-app/src/pages/Signup.js
@@ -16,10 +16,25 @@ const Signup = () => {
         />
       </div>
       {/* text to the right */}
-      <div className="pl-64 justify-center pt-56 font-serif">
+      <div className="pl-64 justify-center pt-36 font-serif">
         <h1 className="font-bold text-6xl">Sign In</h1>
         <p className="font-normal text-lg pt-2">We love our clients</p>
         <form className="flex flex-col mt-12 ">
+        <div className="mb-6">
+            <label
+              htmlFor="Full Name"
+              className="text-white mb-2 block text-sm font-medium"
+            >
+              Full Name
+            </label>
+            <input
+              type="Full Name"
+              id="Full Name"
+              required
+              className="bg-[#18191E] border pl-3 w-full border-[#33353F] placeholder-[#9CA2A9] text-gray  text-lg rounded-lg sm:w-[300px] lg:w-[670px] pt-2.5"
+              placeholder="Full Name"
+            />
+          </div>
           <div className="mb-6">
             <label
               htmlFor="email"
@@ -63,11 +78,17 @@ const Signup = () => {
 
           <button
             type="submit"
-            className="  bg-gray-200 hover:bg-white text-black font-medium py-2.5 rounded-lg sm:w-[300px] lg:w-[670px] "
+            className="  bg-blue-500 hover:bg-white text-black font-medium py-2.5 rounded-lg sm:w-[300px] lg:w-[670px] "
           >
             Create Account
           </button>
         </form>
+        <p class="text-gray-600 pt-8">
+          Already have an account?
+          <a href="LogIn" class="font-bold text-white hover:text-blue-600 pl-2">
+            LogIn
+          </a>{" "}
+        </p>
       </div>
     </div>
   );
