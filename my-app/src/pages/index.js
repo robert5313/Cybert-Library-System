e89ed1b0a1dfae7cb9815d6f71c2fee415d0b453
munyasya21 <munyasyacynthia@gmail.com>
2024-04-13 19:44:41 +0300

    update

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  font-serif justify-between p-10  bg-gradient-to-r from-black to-green-500">
+    <section id="firstSection" className="flex min-h-screen flex-col  font-serif justify-between p-10  bg-gradient-to-r from-black to-green-500">
       <div className="flex text-white">
         <div>
           <Navbar />
@@ -26,23 +26,24 @@ export default function Home() {
           </h1>
           <p className="pt-4">
             Our new library is a game-changer, offering innovative solutions for
-            developers.<br/> With its user-friendly interface and robust features, it
-            simplifies complex tasks <br/>and enhances productivity significantly.
+            developers.
+            <br /> With its user-friendly interface and robust features, it
+            simplifies complex tasks <br />
+            and enhances productivity significantly.
           </p>
           <div className="flex">
-          <div className="pt-4 ">
-                <button className="rounded-lg pt-3 pb-3 pr-5 pl-5 bg-black justify-center items-center whitespace-nowrap ">
-                 <a href=" Signup"> Sign Up</a>
-                </button>
-              </div>
-              <div className="pt-4 pl-4 ">
-                <button className="rounded-lg pt-3 pb-3 pr-5 pl-5 bg-black justify-center items-center whitespace-nowrap ">
-                 <a href="LogIn" >Log In</a>
-                </button>
-              </div>
-              </div>
+            <div className="pt-4 ">
+              <button className="rounded-lg pt-3 pb-3 pr-5 pl-5 bg-black justify-center items-center whitespace-nowrap ">
+                <a href=" Signup"> Sign Up</a>
+              </button>
+            </div>
+            <div className="pt-4 pl-4 ">
+              <button className="rounded-lg pt-3 pb-3 pr-5 pl-5 bg-black justify-center items-center whitespace-nowrap ">
+                <a href="LogIn">Log In</a>
+              </button>
+            </div>
+          </div>
         </div>
-       
 
         {/* image to the right */}
         <div className="pl-56 pt-14 pb-24">
@@ -55,6 +56,20 @@ export default function Home() {
           />
         </div>
       </div>
-    </main>
+      {/* About */}
+      <div className="pl-44 justify-center items-center p-24">
+        <h1 className="text-5xl font-bold ">About Us</h1>
+      </div>
+      <div className="flex pt-8 flex-col min-h-screen w-full pl-24">
+        {/* Image to the left */}
+        <Image src={Book3}
+        width={460}
+        height={460}
+        alt="book img"
+        
+        />
+      </div>
+    </section>
+    
   );
 }
